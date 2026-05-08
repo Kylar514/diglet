6f523726f9c23ea3b561da9aa258ea36c70090e1
zeal <kylar514@gmail.com>
2026-05-08 14:20:30 -0400

    feat: add config w/ yaml parsing

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Connection struct {
+	Name       string `yaml:"name"`
+	TunnelType string `yaml:"tunnel_type"`
+	Namespace  string `yaml:"namespace"`
+	Service    string `yaml:"service"`
+	Container  string `yaml:"container"`
+	SSHHost    string `yaml:"ssh_host"`
+	RemotePort int    `yaml:"remote_port"`
+	LocalPort  int    `yaml:"local_port"`
+}
+
+type Config struct {
+	Connections []Connection `yaml:"connections"`
+}
+
+func DefaultConfigPath() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".config", "diglet", "connections.yaml")
+}
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not read config file: %w", err)
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("could not parse config file: %w", err)
+	}
+
+	return &cfg, nil
+}
